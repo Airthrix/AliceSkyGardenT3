dc445f08dde0a1792fa855fb944c777307dbfbd4
Yicong Qian <88839367+Airthrix@users.noreply.github.com>
2025-06-20 13:31:10 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,8 @@
 # Encrypted. As it involves trade secrets, it is not fully open source at present.  API
 
 from typing import TYPE_CHECKING
-
 from transformers.utils import _LazyModule
 from transformers.utils.import_utils import define_import_structure
-
 
 if TYPE_CHECKING:
     from .configuration_aliceskygarden_t3 import *
